f607c8d817b9ac83b88cbced7141c7ebb35baa2e
turtlekazu <turtlekazu@icloud.com>
2023-07-22 18:33:10 +0900

    Add MainScreen to MainActivity.

--- app/src/main/java/com/example/mvvm_study/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_study/view/MainActivity.kt
@@ -1,15 +1,20 @@
-package com.example.mvvm_study
+package com.example.mvvm_study.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.mvvm_study.ui.theme.Mvvm_studyTheme
+import com.example.mvvm_study.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -19,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CountUp()
+                    MainScreen(mainViewModel = viewModel)
                 }
             }
         }
